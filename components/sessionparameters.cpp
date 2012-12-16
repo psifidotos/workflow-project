06a12f9f3c896a98c923a1b58ff7aa1ae11a0f15
Michail Vourlakos <mvourlakos@gmail.com>
2012-12-16 19:35:45 +0200

    added more session parameters(currentDesktop,noOfDesktops,compositinActive)

--- components/sessionparameters.cpp
+++ components/sessionparameters.cpp
@@ -1,10 +1,21 @@
 #include <sessionparameters.h>
+
+#include <QDBusInterface>
+#include <QDBusConnection>
+
 #include <KActivities/Controller>
+#include <KWindowSystem>
+
 
 SessionParameters::SessionParameters(QObject *parent)
-: QObject(parent), m_controller(new KActivities::Controller(this))
+: QObject(parent),
+  m_controller(new KActivities::Controller(this)),
+  m_kwindowSystem(KWindowSystem::KWindowSystem::self())
 {
     m_currentActivity = m_controller->currentActivity();
+    m_currentDesktop = m_kwindowSystem->currentDesktop();
+    m_numberOfDesktops = m_kwindowSystem->numberOfDesktops();
+    m_effectsSystemEnabled = m_kwindowSystem->compositingActive();
 
     initConnections();
 }
@@ -15,13 +26,18 @@ SessionParameters::~SessionParameters()
         delete m_controller;
 }
 
+
+//QDBusConnection::sessionBus().connect("org.kde.kwin", "/KWin", "org.kde..KWin" ,"compositingToggled", this, SLOT(MySlot(uint)));
 void SessionParameters::initConnections()
 {
-    connect(m_controller, SIGNAL(currentActivityChanged(QString)), this, SLOT(setCurrentActivity(QString)));
+    connect(m_controller, SIGNAL(currentActivityChanged(QString)), this, SLOT(setCurrentActivitySlot(QString)));
+    connect(m_kwindowSystem, SIGNAL(currentDesktopChanged(int)), this, SLOT(setCurrentDesktopSlot(int)));
+    connect(m_kwindowSystem, SIGNAL(numberOfDesktopsChanged(int)), this, SLOT(setNumberOfDesktopsSlot(int)));
+    QDBusConnection::sessionBus().connect("org.kde.kwin", "/KWin", "org.kde.KWin" ,"compositingToggled", this, SLOT(setEffectsSystemEnabledSlot(bool)));
 }
 
 
-void SessionParameters::setCurrentActivity(QString curAct)
+void SessionParameters::setCurrentActivitySlot(QString curAct)
 {
     m_currentActivity = curAct;
     emit currentActivityChanged(m_currentActivity);
@@ -31,5 +47,40 @@ QString SessionParameters::currentActivity()
 {
     return m_currentActivity;
 }
+
+void SessionParameters::setCurrentDesktopSlot(int desktop)
+{
+    m_currentDesktop = desktop;
+    emit currentDesktopChanged(m_currentDesktop);
+}
+
+int SessionParameters::currentDesktop()
+{
+    return m_currentDesktop;
+}
+
+void SessionParameters::setNumberOfDesktopsSlot(int number)
+{
+    m_numberOfDesktops = number;
+    emit numberOfDesktopsChanged(m_numberOfDesktops);
+}
+
+int SessionParameters::numberOfDesktops()
+{
+    return m_numberOfDesktops;
+}
+
+
+void SessionParameters::setEffectsSystemEnabledSlot(bool status)
+{
+    m_effectsSystemEnabled = status;
+    emit numberOfDesktopsChanged(m_effectsSystemEnabled);
+}
+
+bool SessionParameters::effectsSystemEnabled()
+{
+    return m_effectsSystemEnabled;
+}
+
 
 #include <sessionparameters.moc>
--- components/sessionparameters.h
+++ components/sessionparameters.h
@@ -2,7 +2,11 @@
 #define SESSIONPARAMETERS_H
 
 #include <QObject>
+
 #include <KActivities/Controller>
+
+
+class KWindowSystem;
 
 /* A simple wrapper around Environment's parameters */
 class SessionParameters : public QObject
@@ -10,23 +14,39 @@ class SessionParameters : public QObject
     Q_OBJECT
 public:
 
-    Q_PROPERTY(QString currentActivity READ currentActivity WRITE setCurrentActivity NOTIFY currentActivityChanged)
+    Q_PROPERTY(QString currentActivity READ currentActivity NOTIFY currentActivityChanged)
+    Q_PROPERTY(int currentDesktop READ currentDesktop NOTIFY currentDesktopChanged)
+    Q_PROPERTY(int numberOfDesktops READ numberOfDesktops NOTIFY numberOfDesktopsChanged)
+    Q_PROPERTY(bool effectsSystemEnabled READ effectsSystemEnabled NOTIFY effectsSystemChanged)
 
     explicit SessionParameters(QObject *parent = 0);
     ~SessionParameters();
 
     QString currentActivity();
+    int currentDesktop();
+    int numberOfDesktops();
+    bool effectsSystemEnabled();
 
 signals:
     void currentActivityChanged(QString);
+    void currentDesktopChanged(int);
+    void numberOfDesktopsChanged(int);
+    void effectsSystemChanged(int);
 
 public slots:
-    void setCurrentActivity(QString);
+    void setCurrentActivitySlot(QString);
+    void setCurrentDesktopSlot(int);
+    void setNumberOfDesktopsSlot(int);
+    void setEffectsSystemEnabledSlot(bool);
 
 private:
     KActivities::Controller *m_controller;
+    KWindowSystem *m_kwindowSystem;
 
     QString m_currentActivity;
+    int m_currentDesktop;
+    int m_numberOfDesktops;
+    bool m_effectsSystemEnabled;
 
     void initConnections();
 };
